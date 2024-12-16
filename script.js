51d355857e922761b8a97d726ed3916dc12d7233
kavitha sree <kavithasreelavan2005@gmail.com>
2024-12-16 12:11:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Scroll Animation
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll('.section');
+    hiddenElements.forEach(el => observer.observe(el));
+});
